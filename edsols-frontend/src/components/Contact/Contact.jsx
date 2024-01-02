c779930c5e30ea7912b0e07ebdcf41066c07e17c
Sammy-05 <m.sarim12966@gmail.com>
2024-01-02 12:29:04 +0500

    Fixed dimensions

--- edsols-frontend/src/components/Contact/Contact.jsx
+++ edsols-frontend/src/components/Contact/Contact.jsx
@@ -3,11 +3,11 @@ const Contact = () => {
     <div>
 
       <div className="flex flex-col items-center justify-center text-xl font-bold pt-24">
-      <div className="flex justify-center">
+      <div className="flex justify-center z-0">
         <div className="absolute rounded-t-[105px] border-t-4 border-l-4 border-r-4 border-solid border-secondary w-3/5 h-[30%]"></div>
         <div className="absolute rounded-t-[105px] border-t-4 border-l-4 border-r-4 border-solid border-heading w-2/3 h-[30%]"></div>
       </div>
-      <div className="flex flex-col items-center pt-16">
+      <div className="flex flex-col items-center pt-16 z-10">
         <div>
           <h1 className="text-heading">Contact</h1>
         </div>
